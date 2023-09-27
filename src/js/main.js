058c8bff5608b70ade1a76b378154cc1cfd9d74e
jasper schoormans <jasper.schoormans@gmail.com>
2023-09-27 14:40:14 +0200

    added our own layers - this is really cool

--- src/js/main.js
+++ src/js/main.js
@@ -3,21 +3,31 @@
 /*
  * Initialize map
  */
-var viewCenter = [37.0, -92.5];
-var viewSouthWest = L.latLng(24.0, -125.0);
-var viewNorthEast = L.latLng(50.0, -60.0);
+var viewCenter = [51.9133, 4.4947];
+var viewSouthWest = L.latLng(0, -125.0);
+var viewNorthEast = L.latLng(80.0, 60.0);
 var viewBounds = L.latLngBounds(viewSouthWest, viewNorthEast);
 var map = L.map('map', {
-    zoom: 5,
-    minZoom: 4,
-    center: viewCenter,
-    maxBounds: viewBounds
+	zoom: 19,
+	minZoom: 4,
+	maxZoom: 25,
+	center: viewCenter,
+	maxBounds: viewBounds,
 });
-var baseLayer = L.tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
-var compareLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+var baseLayer = L.tileLayer('https://service.pdok.nl/hwh/luchtfotorgb/wmts/v1_0/2023_quick_orthoHR/EPSG:3857/{z}/{x}/{y}.png', {
+	maxNativeZoom: 20, // set the maximum zoom level at which the tiles are available
+	maxZoom: 25, // set the maximum zoom level at which the map can be displayed
+});
+
+
+var compareLayer = L.tileLayer('https://api.maptiler.com/tiles/0bd4e0b2-fd9e-40be-be22-052cf984b5c6/{z}/{x}/{y}.png?key=hV04Qt1KNH7EvsutoWv4', {
+	maxNativeZoom: 21, // set the maximum zoom level at which the tiles are available
+	maxZoom: 25, // set the maximum zoom level at which the map can be displayed
+});
+
 baseLayer.addTo(map);
 compareLayer.addTo(map);
-
 /*
  * Add Layer swiper
  */
